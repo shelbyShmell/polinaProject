d0a81015c6a47bdb7bf424a3ad0b0524a916fc79
Полина Понарина <misakiii1@mail.ru>
2025-01-20 01:51:30 +0500

    generalStatistics

--- myproject/main/urls.py
+++ myproject/main/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.profession, name="profession"),
-  
+    path("genstatistic/", views.statistic, name="statistic"),
 ]
--- myproject/main/views.py
+++ myproject/main/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def profession(request):
     return render(request, "main/main-page.html")
+
+def statistic(request):
+    return render(request, "main/general-statistics.html")
